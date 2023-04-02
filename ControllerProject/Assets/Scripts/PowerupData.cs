93ff8bddbaf9d5c7edabdd9b9437158555d428af
Karkaranos <karkaranosyt@gmail.com>
2023-04-02 18:49:29 -0500

    Framework for PowerUps and Attacks

---
+++ ControllerProject/Assets/Scripts/PowerupData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PowerupData", menuName = "Power-Up Data")]
+public class PowerupData : ScriptableObject
+{
+
+}
---
+++ ControllerProject/Assets/SheriffBulletBehavior.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheriffBulletBehavior : MonoBehaviour
+{
+    [SerializeField] private GameObject scope;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set direction
+    }
+}
