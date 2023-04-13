e7ccd8cc9b510dd78aab0ac44d22429ee6621714
Karkaranos <karkaranosyt@gmail.com>
2023-04-13 12:16:47 -0500

    Character No Blinkie

--- ControllerProject/Assets/Scripts/CharacterScripts/Sheriff/SheriffArt.cs
+++ ControllerProject/Assets/Scripts/CharacterScripts/Sheriff/SheriffArt.cs
@@ -15,19 +15,19 @@ public class SheriffArt : MonoBehaviour
     {
         if (pos.x > 0.05 && pos.x < 1)
         {
-            GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+            //GetComponent<Renderer>().material.color = new Color(255, 0, 0);
         }
         else if (pos.x < -0.05 && pos.x > -1)
         {
-            GetComponent<Renderer>().material.color = new Color(255, 255, 0);
+            //GetComponent<Renderer>().material.color = new Color(255, 255, 0);
         }
         else if (pos.y > 0 && pos.y < 1)
         {
-            GetComponent<Renderer>().material.color = new Color(0, 0, 255);
+            //GetComponent<Renderer>().material.color = new Color(0, 0, 255);
         }
         else if (pos.y < 0 && pos.y > -1)
         {
-            GetComponent<Renderer>().material.color = new Color(0, 255, 255);
+            //GetComponent<Renderer>().material.color = new Color(0, 255, 255);
         }
 
     }
